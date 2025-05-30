632151898cb89dee9dfcc41bd628943e4f636eee
OrleyZambrano <e1316661675@live.uleam.edu.ec>
2025-05-29 23:14:37 -0500

    Implementar aplicación Flask básica y agregar dependencia en requirements.txt

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hola Mundo")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '¡Hola Mundo desde la web!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
