ce20b5f40b4726d734594e5f495fb820643dae93
OrleyZambrano <e1316661675@live.uleam.edu.ec>
2025-05-29 23:32:26 -0500

    Eliminar flujo de trabajo de despliegue de Azure, agregar Procfile y actualizar main.py y requirements.txt

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return '¡Hola Mundo desde la web!'
+def hello():
+    return '¡Hola Mundo desde Azure Web App con Python 3.13!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000)
